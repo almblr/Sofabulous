29028c14fe08db1c5cbd3017e60d8d19a26319cd
Mehdi <birkyo@gmail.com>
2022-05-24 19:26:11 +0200

    début panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,8 @@
-let monTableau = JSON.parse(localStorage.getItem(`product_list`));
-console.log(monTableau[0]);
+let monTableau = JSON.parse(localStorage.getItem(`product_list`)); // JSON.Parse transforme un string en objet JSON
+const sectionItem = document.querySelector("#cart__items");
+let monApi = "http://localhost:3000/api/products/";
 
-
-
+for (let idItem of monTableau) {
+    idItem = idItem.id;
+    console.log(idItem);
+}
