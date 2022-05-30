2cdabd291e790c29e1cebd338798d0a3179b6c2b
Mehdi <birkyo@gmail.com>
2022-05-30 17:32:40 +0200

    product js w/ addtion id/color

--- front/js/product.js
+++ front/js/product.js
@@ -1,21 +1,45 @@
-////////////////////////// FICHE PRODUIT //////////////////////////
-////////////////////////// FICHE PRODUIT //////////////////////////
-////////////////////////// FICHE PRODUIT //////////////////////////
-
-
-
 let monApi = "http://localhost:3000/api/products/";
+const str = window.location; // Return l'URL actuelle sous forme de str(ing)
+const url = new URL(str); // vv searchParams vv fonctionne avec un url
+const idProduit = url.searchParams.get("id"); // Récup' la partie ID du lien
+monApi += `${idProduit}`; // return API de base + ID Produit
+
 const image = document.querySelector(".item__img");
 const title = document.querySelector("#title");
 const price = document.querySelector("#price");
 const description = document.querySelector("#description");
 const couleurs = document.querySelector("#colors");
-const str = window.location; // Return l'URL actuelle sous forme de str(ing)
-const url = new URL(str); // vv searchParams vv fonctionne avec un url
-const idProduit = url.searchParams.get("id"); // Récup' la partie ID du lien
+const bouton = document.querySelector("#addToCart");
 
+let product = { // Produit "vide"
+    id : 0,
+    qty : 0,
+    color: ""
+};
 
-monApi += `${idProduit}`; // return API de base + ID Produit
+// Créé & remplace les variables des infos produits de façon dynamique avec les éléments déjà existants sur la page (price qty color) et ceux de l'api (id product)
+function ProductInfo(productObject, productData) {
+    let id = productData._id;
+    let qty = parseInt(document.querySelector("#quantity").value); // parseInt : Convert type str into type nbr
+    let colour = document.querySelector("#colors").value;
+    productObject.id = id;
+    productObject.qty=qty;
+    productObject.color=colour;
+    return productObject;
+};
+
+/**
+ * Ajoute les items dans le localstorage
+ * @param {array} tabLocStorage - Tableau d'objets local storage
+ * @param {*} tabData 
+ * @param {*} productData 
+ */
+function addToLocalStorage(tabLocStorage, tabData, productData) {
+    tabLocStorage.push(ProductInfo(productData, tabData)); // push l'objet dans le tableau 
+    localStorage.setItem(`product_list`, JSON.stringify(tabLocStorage)); // met les données du produit dans le localstorage
+    alert("L'article a bien été ajouté dans votre panier.")
+    //json.stringify pour stocker des objets sous forme json
+};
 
 fetch(monApi)
     .then((response) => response.json())
@@ -30,60 +54,15 @@ fetch(monApi)
             newOption.innerText = color;
             couleurs.appendChild(newOption);
         };
-    })
-    .catch(erreur => console.log("Error 4 sans Kanap"));
-
-////////////////////////// ADD TO CART //////////////////////////
-////////////////////////// ADD TO CART //////////////////////////
-////////////////////////// ADD TO CART //////////////////////////
-
-const bouton = document.querySelector("#addToCart");
-
-let product = {
-    id : 0,
-    qty : 0,
-    color: ""
-};
-
-// productInfo créé & remplace les variables des infos produits (prix, couleur...) de façon dynamique
-function ProductInfo(productObject, productData) {
-    let id = productData._id;
-    let qty = parseInt(document.querySelector("#quantity").value); // parseInt : Tranform type str en type number
-    let colour = document.querySelector("#colors").value;
-    productObject.id = id;
-    productObject.qty=qty;
-    productObject.color=colour;
-    return productObject;
-};
-
-bouton.addEventListener("click", function() {
-    if (localStorage.getItem("product_list") === null) { // si le localstorage ne contient pas la clé product_list
-        let tabProduct = []; // je créé le tableau qui servira de valeur à cette clé
-        fetch(monApi)
-            .then((response) => response.json())
-            .then((data) => {
-                tabProduct.push(ProductInfo(product, data)); // push l'objet dans le tableau 
-                localStorage.setItem(`product_list`, JSON.stringify(tabProduct)); // met les données du produit dans le localstorage
-                alert("L'article a bien été ajouté dans votre panier.")
-            }) //json.stringify pour stocker des objets sous forme json
-            .catch(erreur => console.log("Error 4 sans Kanap"));
-    } else {  // Sinon, ajoute des objets (produit) dans le tableau existants
-        fetch(monApi)
-            .then((response) => response.json())
-            .then((data) => {
+        bouton.addEventListener("click", function() {
+            if (localStorage.getItem("product_list")) {  
                 let tabProduct = JSON.parse(localStorage.getItem(`product_list`)); // Récup le contenu du localstorage sous format JSON (il est en string de base)
-                tabProduct.push(ProductInfo(product, data));
-                localStorage.setItem(`product_list`, JSON.stringify(tabProduct));
-                alert("L'article a bien été ajouté dans votre panier.")
-            })
-        .catch(erreur => console.log("Error 4 sans Kanap"));
-    }
-});
+                addToLocalStorage(tabProduct, data, product);
+            }
+            if (localStorage.getItem("product_list") === null) { // si le localstorage ne contient pas la clé product_list
+                let tabProduct = []; // je créé le tableau qui servira de valeur à cette clé
+                addToLocalStorage(tabProduct, data, product);
+            } 
 
-/// Ne pas avoir de doublon, additioner les quantités pour chaque item
-
-
-//// essayer de faire fonctionner le code avec unseul fetch plutôt que trois (enregistrer les données et les re use)
-/// fusion des produis : array.find
-
-/// rebooser l'index et product avant de partir sur cart
+        })
+    });     
--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,41 @@
-const monApi = "http://localhost:3000/api/products/"; // On déclare une variable pour stocker l'API
+let monApi = "http://localhost:3000/api/products/"; // On déclare une variable pour stocker l'API
 const section = document.querySelector("#items");
 
-function addElement(i) {
-    const newLink = document.createElement("a");
-    newLink.setAttribute("href", `product.html?id=${i._id}`);
-    const newArticle = document.createElement("article");
-    const newImg = document.createElement("img");
-    newImg.setAttribute("src", `${i.imageUrl}`);
-    newImg.setAttribute("alt", `${i.altTxt}`);
-    const newTitle = document.createElement("h3");
-    const newParagraph = document.createElement("p");
-    newParagraph.innerText = i.description;
-    section.appendChild(newLink);
-    newLink.appendChild(newArticle);
-    newArticle.appendChild(newImg);
-    newArticle.appendChild(newTitle);
-    newArticle.appendChild(newParagraph);
-};
+// function addElement(i) {
+//     const newLink = document.createElement("a");
+//     newLink.setAttribute("href", `product.html?id=${i._id}`);
+//     const newArticle = document.createElement("article");
+//     const newImg = document.createElement("img");
+//     newImg.setAttribute("src", `${i.imageUrl}`);
+//     newImg.setAttribute("alt", `${i.altTxt}`);
+//     const newTitle = document.createElement("h3");
+//     const newParagraph = document.createElement("p");
+//     newParagraph.innerText = i.description;
+//     section.appendChild(newLink);
+//     newLink.appendChild(newArticle);
+//     newArticle.appendChild(newImg);
+//     newArticle.appendChild(newTitle);
+//     newArticle.appendChild(newParagraph);
+// };
 
 fetch(monApi)
     .then((response) => response.json())
     .then((data) => {
         for (let item of data) {
-            addElement(item);
+            const newLink = document.createElement("a");
+            newLink.setAttribute("href", `product.html?id=${item._id}`);
+            const newArticle = document.createElement("article");
+            const newImg = document.createElement("img");
+            newImg.setAttribute("src", `${item.imageUrl}`);
+            newImg.setAttribute("alt", `${item.altTxt}`);
+            const newTitle = document.createElement("h3");
+            const newParagraph = document.createElement("p");
+            newParagraph.innerText = item.description;
+            section.appendChild(newLink);
+            newLink.appendChild(newArticle);
+            newArticle.appendChild(newImg);
+            newArticle.appendChild(newTitle);
+            newArticle.appendChild(newParagraph);
+        };
     }
-});
+);
