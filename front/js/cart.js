f8312d983e45f5d48d8a978e5dbca2239ecae886
Mehdi <birkyo@gmail.com>
2022-06-06 19:26:46 +0200

    function getPrice

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,6 +7,7 @@ function getLocalStorage() {
     let content = JSON.parse(localStorage.getItem(`product_list`));
     return content;
 };
+
 // fillCart sert à remplir la page panier avec les produits ajoutés au LS
 function fillCart() {
     let promises = [];
@@ -29,7 +30,7 @@ function fillCart() {
                     <div class="cart__item__content__description">
                         <h2>${data.name}</h2>
                         <p>${product.color}</p>
-                        <p>${data.price}</p>
+                        <p>${data.price}€</p>
                     </div>
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
@@ -51,11 +52,12 @@ function fillCart() {
 
 const promises = fillCart();
 
-Promise.all(promises).then(() => { // nous dit quand ttes les promesses sont terminées et lance le .then si c'est le cas
+Promise.all(promises).then(() => { // Return .then si ttes les promesses sont réussies/terminées
     console.log("La quantité totale est de " + totalQuantity() + " canapé(s)");
     changeQuantity();
     deleteItem();
     totalQuantity();
+    console.log(totalPrice());
 });
 
 /**
@@ -68,14 +70,14 @@ Promise.all(promises).then(() => { // nous dit quand ttes les promesses sont ter
 };
 
 function changeQuantity() {
-    let qtyInputs = document.querySelectorAll(".itemQuantity");
-    qtyInputs.forEach(inputItem => {
-        inputItem.addEventListener("change", () => {
-        let itemQty = inputItem.value;
+    let allQtyInputs = document.querySelectorAll(".itemQuantity");
+    allQtyInputs.forEach(itemInput => { // pour chaque input parmi allQtyInputs
+        itemInput.addEventListener("change", () => {
         let arrayLS = getLocalStorage();
-        let prodID = inputItem.closest("article").dataset.id; // stock l'id de l'article le plus proche
-        let prodColor = inputItem.closest("article").dataset.color;
-        const element = arrayLS.find(produit => produit.id == prodID && produit.color == prodColor); 
+        let itemQty = itemInput.value;
+        let itemID = itemInput.closest("article").dataset.id; // get dataset.id of the closest <article>
+        let itemColor = itemInput.closest("article").dataset.color; // idem for color
+        let element = arrayLS.find(product => product.id == itemID && product.color == itemColor); 
         let indexItem = arrayLS.indexOf(element)
         arrayLS[indexItem].qty = itemQty;
         saveBasket("product_list", arrayLS);
@@ -85,43 +87,30 @@ function changeQuantity() {
 
 function deleteItem() {
     let deleteButtons = document.querySelectorAll(".deleteItem");
-    deleteButtons.forEach(button => { // chaque bouton .deleteItem
-        button.addEventListener("click", () => { // au clic
-        let prodID = button.closest("article").dataset.id; // stock l'id de l'article le plus proche
-        let prodColor = button.closest("article").dataset.color;
-        button.closest("article").remove(); // supprimera l'élément le plus proche du bouton qui a article[data-id]
+    deleteButtons.forEach(button => { // pour chaque bouton parmi deleteButtons
+        button.addEventListener("click", () => {
+        let itemID = button.closest("article").dataset.id; 
+        let itemColor = button.closest("article").dataset.color;
+        button.closest("article").remove(); // delete the closest <article> (dom only)
+        // Suppression au niveau du local storage
         let arrayLS = getLocalStorage();
-        const element = arrayLS.find(produit => produit.id == prodID && produit.color == prodColor); 
-        arrayLS.splice(arrayLS.indexOf(element), 1); // a partir de l'index arrayLS.indexOf(element), sélectionne 1 index (donc lui-même)
+        let element = arrayLS.find(product => product.id == itemID && product.color == itemColor); 
+        let productIdx = arrayLS.indexOf(element) // Get l'index du produit en question
+        arrayLS.splice(productIdx, 1); // Select one element from productIdx & delete it (donc lui-même)
         saveBasket("product_list", arrayLS)  
         })
     })
 }
 
-
-
-
-function totalQuantity() {
-    let inputsItems = Array.from(document.querySelectorAll(".itemQuantity")); // array.from car de base c'est un HTMLcollection
-    let sum = 0;
-    inputsItems.forEach(inputItem => {
-        let values = parseInt(inputItem.value) // une valeur d'input sera TOUJOURS un string donc faut convertir
-        sum += values;
-    })
-    return sum;
+function totalPrice() {
+    return itemPrice = document.querySelector(".cart__item__content__description >:nth-child(3)").textContent.replace("€","")
 }
 
 
+function totalQuantity() {
+    let arrayLS = getLocalStorage();
+    return arrayLS.map(item => parseInt(item.qty)).reduce((acc, i) => acc + i);
+}
 
-// if (element.value) {
-//     console.log(element.value);
-
-// function totalQuantity() {
-//     totalQuantity = 
-// }
-// function totalPrice(qtyProduct, priceProduct) {
-//     let totalPrice = qtyProduct*priceProduct;
-//     return totalPrice
-// }
 
 // getElementByClassName est une interface de collection d'élements qui ressemble à un tableau mais n'est pas pas vraiment un. Du coup faut d'abord transformer le contenu en tableau avec [...elements].forEach ou Array.from(elements).forEach()
