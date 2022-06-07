0c6de888d87ac5c3092ab3afecda39929ee19e1d
Mehdi <birkyo@gmail.com>
2022-06-07 17:20:40 +0200

    refacto

--- front/js/cart.js
+++ front/js/cart.js
@@ -31,7 +31,7 @@ function fillCart() {
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
                             <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qty}">
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qty}" onKeyDown="return false">
                         </div>
                         <div class="cart__item__content__settings__delete">
                             <p class="deleteItem">Supprimer</p>
@@ -53,8 +53,8 @@ const promises = fillCart();
 Promise.all(promises).then(() => { // Return .then si ttes les promesses sont réussies/terminées
     changeQuantity();
     deleteItem();
-    getTotalQuantity()
-    console.log("Le prix total est de : " + getTotalPrice() +"€");
+    getTotalPrice();
+    getTotalQuantity();
 });
 
 // Permet de mettre à jour le contenu du LS (le panier)
@@ -66,7 +66,7 @@ function saveBasket(key, tab) {
 function getProductIndex(item) {
     let itemID = item.closest("article").dataset.id; // get dataset.id of the closest <article>
     let itemColor = item.closest("article").dataset.color; // idem for color
-    let productIdx = contentLS.findIndex(product => product.id == itemID && product.color == itemColor); // Compare les ID et les couleurs des produits et sélectionne ceux qui correspondent pour avoir produit DOM = produit LS. indexOf permet 
+    let productIdx = contentLS.findIndex(product => product.id == itemID && product.color == itemColor); // Compare les ID et les couleurs des produits et sélectionne ceux qui correspondent pour avoir produit DOM = produit LS.
     return productIdx;
 }
 
@@ -78,6 +78,8 @@ function changeQuantity() {
             let itemQty = itemInput.value;
             contentLS[getProductIndex(itemInput)].qty = itemQty; // La qty du LS prend la valeur de l'input
             saveBasket("product_list", contentLS);
+            getTotalPrice();
+            getTotalQuantity();
         })
     })
 }
@@ -90,31 +92,38 @@ function deleteItem() {
             button.closest("article").remove(); // DOM - delete the closest <article> 
             contentLS.splice(getProductIndex(button), 1); // LS - Select one element from productIdx & delete it (donc lui-même) 
             saveBasket("product_list", contentLS)  
+            getTotalQuantity();
         })
     })
 }
 
 // Calcul du prix total en fonction du prix de chaque item par rapport à sa qty
 function getTotalPrice() {
-    let arr = Array.prototype.slice.call(document.querySelectorAll(".cart__item__content__description >:nth-child(3)"))
-    // Passe le résultat de querySelectorAll de nodelist à un array  
-    let arrPrices = [] // On initialiser un tableau vide pour stocker les prix
-    for (let itemPrice of arr) {
-        let itemQty = contentLS[getProductIndex(itemPrice)].qty; // On récupère la quantité de l'article
-        arrPrices.push((parseInt(itemPrice.textContent.replace("€","")))*itemQty);
-        // Push in arrPrices le text.content de chaque prix (en remplaçant € par rien) en mutipliant par la quantité du produit tout en convertissant le tout en type number
-    }
-    return arrPrices.reduce((acc, x) => acc + x) // Calcul du prix total
+    if (contentLS.length != 0) {
+        let arr = document.querySelectorAll(".cart__item__content__description >:nth-child(3)");
+        // Passe le résultat de querySelectorAll de nodelist à un array  
+        let arrPrices = [] // On initialiser un tableau vide pour stocker les prix
+        for (let itemPrice of arr) {
+            let itemQty = contentLS[getProductIndex(itemPrice)].qty; // On récupère la quantité de l'article
+            arrPrices.push((parseInt(itemPrice.textContent)*itemQty));
+            // Push in arrPrices le text.content de chaque prix en mutipliant par la quantité du produit tout en convertissant le tout en type number
+        }
+        document.getElementById("totalPrice").innerText = arrPrices.reduce((acc, x) => acc + x) // Calcul du prix total
+    } else {
+        getTotalQuantity()  
+    }      
 }
 
 function getTotalQuantity() {
     if (contentLS.length == 0) {
-        console.log("Votre panier est vide");
+        document.getElementById("totalQuantity").innerText = "0";
+        document.getElementById("totalPrice").innerText = "0";
     } else {
-        let totalQty = contentLS.map(item => parseInt(item.qty)).reduce((acc, i) => acc + i);
-        console.log("La quantité totale est de " + totalQty + " canapé(s)");
+        document.getElementById("totalQuantity").innerText = contentLS.map(item => parseInt(item.qty)).reduce((acc, i) => acc + i);
     }
 }
 
 
 // getElementByClassName est une interface de collection d'élements qui ressemble à un tableau mais n'est pas pas vraiment un. Du coup faut d'abord transformer le contenu en tableau avec [...elements].forEach ou Array.from(elements).forEach()
+
+//parseInt exclut tout ce qui n'est pas un chiffre genre c'est le Zemmour des lettres XD du coup pas besoin de replace("€", "") pour virer le signe € dans le prix
